d79a76c3ede5b83798814ac6a6b7d7d8535fd253
Michael <michaelbarr24@hotmail.com>
2022-07-28 08:34:21 +0100

    Add connectiong to MongoDB

--- api-climbing/index.js
+++ api-climbing/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-require("./startup/routes")(app);
 const {infoLog} = require('./startup/logger')
+require('dotenv').config()
+require("./startup/routes")(app);
 require("./startup/db")()
 
 app.listen(port, () => {
--- api-climbing/startup/db.js
+++ api-climbing/startup/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const {infoLog, errorLog}=require('./logger')
 
 const db = ()=> {
-    mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASSWORD}@climbing.p5ug7c5.mongodb.net/?retryWrites=true&w=majority`, ()=> {
-        console.log("Connected")
-    })
+    mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASSWORD}@climbing.p5ug7c5.mongodb.net/?retryWrites=true&w=majority`, (err)=>{
+        err === null ? infoLog("Connected to MongoDB"): errorLog(err)
+    }).catch(err=> errorLog(err))
 }
 
 module.exports = db
