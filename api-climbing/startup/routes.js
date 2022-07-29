b822a877ffe69b4f5cf238f27c20515276770a45
Michael B <michaelbarr24@hotmail.com>
2022-07-29 19:04:43 +0100

    Add contact route with nodemailer

--- api-climbing/startup/routes.js
+++ api-climbing/startup/routes.js
@@ -1,5 +1,6 @@
 const users = require("../routes/users");
 const crags = require("../routes/crags");
+const contact = require("../routes/contact");
 const express = require("express");
 const { errorReqLog } = require("../startup/logger");
 
@@ -7,5 +8,6 @@ module.exports = (app) => {
   app.use(express.json());
   app.use("/api/users", users);
   app.use("/api/crags", crags);
+  app.use("/api/contact", contact);
   app.use(errorReqLog);
 };
