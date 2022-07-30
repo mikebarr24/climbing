78886358db19aa32cb3d577f9fac28cf255a593a
Michael B <michaelbarr24@hotmail.com>
2022-07-30 22:32:32 +0100

    Add basic Home section

--- frontend-climb/src/components/Home/Home.js
+++ frontend-climb/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.scss";
+const logo = require("../../media/images/climbing-ni-logo.png");
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <img src={logo} alt="climbingNI logo" className="home-logo" />
+      <h2 className="subheading">Everything Climb in Northern Ireland</h2>
     </div>
   );
 }
