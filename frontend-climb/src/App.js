deabaef6e4a90835dd13e7e87e1143a5cc3b797d
Michael B <michaelbarr24@hotmail.com>
2022-07-30 09:06:17 +0100

    Add react router and Homepage

--- frontend-climb/src/App.js
+++ frontend-climb/src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Working</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
