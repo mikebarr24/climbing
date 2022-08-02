18bd78ec1c7e4ff8343176932470b417d86892ef
Michael B <michaelbarr24@hotmail.com>
2022-08-02 16:02:08 +0100

    Add cors

--- api-climbing/startup/routes.js
+++ api-climbing/startup/routes.js
@@ -3,9 +3,11 @@ const crags = require("../routes/crags");
 const contact = require("../routes/contact");
 const express = require("express");
 const { errorReqLog } = require("../startup/logger");
+const cors = require("cors");
 
 module.exports = (app) => {
   app.use(express.json());
+  app.use(cors());
   app.use("/api/users", users);
   app.use("/api/crags", crags);
   app.use("/api/contact", contact);
