ac3ebe97e9fb44c3818b41cec409da4a85e1e719
Michael B <michaelbarr24@hotmail.com>
2022-08-02 07:07:00 +0100

    Add button for Home

---
+++ frontend-climb/src/components/Button/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Button.scss";
+
+function Button(props) {
+  let styles;
+  if (props.color === "light") {
+    styles = {
+      backgroundColor: "#fff",
+      color: "#000",
+    };
+  } else {
+    styles = {
+      backgroundColor: "#000",
+      color: "#fff",
+    };
+  }
+  return (
+    <>
+      <button className={`btn-home ${props.className}`} style={styles}>
+        {props.name}
+      </button>
+    </>
+  );
+}
+
+export default Button;
