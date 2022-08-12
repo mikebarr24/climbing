6cc423968a3011c3d68d30363675cccb3d4cf276
Michael B <michaelbarr24@hotmail.com>
2022-08-12 21:46:43 +0100

    Add crags pages

---
+++ frontend-climb/src/components/Crags/Crags.js
@@ -0,0 +1,13 @@
+import "./Crags.scss";
+
+import React from "react";
+
+function Crags() {
+  return (
+    <div id="crags" className="container">
+      <h2 className="page-title">Crags</h2>
+    </div>
+  );
+}
+
+export default Crags;
