24391d2d5efd6967047210e0bbd943af0ea6c3a7
Michael B <michaelbarr24@hotmail.com>
2022-08-30 07:39:08 +0100

    Update map is now working with api

--- frontend-climb/src/components/Crags/Map.jsx
+++ frontend-climb/src/components/Crags/Map.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import ApiKeys from "../../api/ApiKeys";
 
 function Map() {
+  const [api, setApi] = useState(null);
+  useEffect(() => {
+    const getApi = async () => {
+      const { data } = await ApiKeys.mapsApi();
+      setApi(data);
+    };
+    getApi();
+  }, []);
   return (
-    <GoogleMap
-      zoom={7}
-      center={{ lat: 54.677809, lng: -6.774634 }}
-      mapContainerClassName="map-container"
-    ></GoogleMap>
+    //Replace with api
+    <LoadScript googleMapsApiKey={null}>
+      <GoogleMap
+        zoom={7}
+        center={{ lat: 54.677809, lng: -6.774634 }}
+        mapContainerClassName="map-container"
+      ></GoogleMap>
+    </LoadScript>
   );
 }
 
