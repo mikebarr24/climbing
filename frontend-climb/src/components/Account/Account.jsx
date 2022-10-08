0199f8ee2b67a41aa64a94af2e0fd0349c1b2e88
Michael <michaelbarr24@hotmail.com>
2022-10-08 19:53:08 +0100

    Add account to nav options

--- frontend-climb/src/components/Account/Account.jsx
+++ frontend-climb/src/components/Account/Account.jsx
@@ -10,9 +10,24 @@ function Account({ user }) {
             Hello {user.name.charAt(0).toUpperCase() + user.name.slice(1)}
           </h2>
           <div className="account--user-details standard-text">
-            <p>Name - {user.name}</p>
-            <p>Name - {user.email}</p>
-            <p>Admin Account - {user.isAdmin === true ? "Yes" : "No"}</p>
+            <p>
+              <strong>Name</strong> - {user.name}
+            </p>
+            <p>
+              <strong>Email</strong> - {user.email}
+            </p>
+            <p>
+              <strong>Admin Account</strong> -{" "}
+              {user.isAdmin === true ? "Yes" : "No"}
+            </p>
+            <div className="account--password-change-wrapper">
+              <p>
+                <strong>Password</strong> -{" "}
+              </p>{" "}
+              <button className="account--change-password-btn">
+                Change Password
+              </button>
+            </div>
           </div>
         </>
       )}
