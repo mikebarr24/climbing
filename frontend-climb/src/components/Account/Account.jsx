8c1abfaaac16cd84d08cd36fae20b54da6abb0d3
Michael Barr <michaelbarr24@hotmail.com>
2022-10-09 11:14:01 +0100

    Update UI for account page

--- frontend-climb/src/components/Account/Account.jsx
+++ frontend-climb/src/components/Account/Account.jsx
@@ -1,6 +1,7 @@
 import { VscAccount } from "react-icons/vsc";
 import { AiOutlineMail } from "react-icons/ai";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { MdOutlineEdit } from "react-icons/md";
 import Button from "../Button/Button";
 import "./Account.scss";
 const profileImage = require("../../media/images/profile-temp-small.jpg");
@@ -11,25 +12,36 @@ function Account({ user }) {
       <div className="background"></div>
       <h2 className="account--name-heading">{user && user.name}</h2>
       <div className="account--info-wrapper standard-text">
-        <img
-          src={profileImage}
-          className="account--image"
-          alt="default profile"
-        />
+        <div className="account--image-wrapper">
+          <span className="account--pencil">
+            <MdOutlineEdit />
+          </span>
+          <img
+            src={profileImage}
+            className="account--image"
+            alt="default profile"
+          />
+        </div>
         <div className="account--info-box">
-          <VscAccount />
+          <span className="account--icon">
+            <VscAccount />
+          </span>
           {user && user.name}
         </div>
         <div className="account--info-box">
-          <AiOutlineMail />
+          <span className="account--icon">
+            <AiOutlineMail />
+          </span>
           {user && user.email}
         </div>
         <div className="account--info-box">
-          <RiLockPasswordLine />
+          <span className="account--icon">
+            <RiLockPasswordLine />
+          </span>
           Password
         </div>
       </div>
-      <Button name="Edit Profile" />
+      <Button name="Edit Profile" className="account--edit-btn" />
     </div>
   );
 }
