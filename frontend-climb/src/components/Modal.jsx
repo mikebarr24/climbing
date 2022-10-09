6ceb4d9e437412051c4ded4a6bb96649ab6f6191
Michael Barr <michaelbarr24@hotmail.com>
2022-10-09 11:44:39 +0100

    Add common close button

--- frontend-climb/src/components/Modal.jsx
+++ frontend-climb/src/components/Modal.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./Modal.scss";
 import Button from "./Button/Button";
 import { AiOutlineCloudUpload } from "react-icons/ai";
+import CloseButton from "./common/CloseButton";
 
 function Modal({ open, sector, close }) {
   const initForm = {
@@ -34,9 +35,7 @@ function Modal({ open, sector, close }) {
     <>
       <div style={OVERLAY} />
       <div className="crag-modal standard-text">
-        <button onClick={close} className="close-button">
-          X
-        </button>
+        <CloseButton onClick={close} />
         <h2>Sector - {sector.sectorName}</h2>
         <div className="sector-photo">Photo of Sector here</div>
         <h3>Sector Info</h3>
